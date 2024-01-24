0bc6bf74628998d1bcffe0b2cf49466ccb1e1807
Rely Ky <relyky@hotmail.com>
2024-01-24 14:21:25 +0800

    送出查詢指令 UI

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,27 +29,72 @@
     private void InitializeComponent()
     {
       panel1 = new Panel();
+      labelResult = new Label();
+      label1 = new Label();
+      cboStatus = new ComboBox();
+      button2 = new Button();
       button1 = new Button();
       backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
       webAgent = new Microsoft.Web.WebView2.WinForms.WebView2();
-      button2 = new Button();
+      progressBar = new ProgressBar();
       panel1.SuspendLayout();
       ((System.ComponentModel.ISupportInitialize)webAgent).BeginInit();
       SuspendLayout();
       // 
       // panel1
       // 
+      panel1.Controls.Add(progressBar);
+      panel1.Controls.Add(labelResult);
+      panel1.Controls.Add(label1);
+      panel1.Controls.Add(cboStatus);
       panel1.Controls.Add(button2);
       panel1.Controls.Add(button1);
       panel1.Dock = DockStyle.Top;
       panel1.Location = new Point(0, 0);
       panel1.Name = "panel1";
-      panel1.Size = new Size(800, 70);
+      panel1.Size = new Size(800, 90);
       panel1.TabIndex = 0;
+      // 
+      // labelResult
+      // 
+      labelResult.AutoSize = true;
+      labelResult.Location = new Point(378, 20);
+      labelResult.Name = "labelResult";
+      labelResult.Size = new Size(51, 19);
+      labelResult.TabIndex = 4;
+      labelResult.Text = "label2";
+      // 
+      // label1
+      // 
+      label1.AutoSize = true;
+      label1.Location = new Point(12, 20);
+      label1.Name = "label1";
+      label1.Size = new Size(52, 19);
+      label1.TabIndex = 3;
+      label1.Text = "Status";
+      // 
+      // cboStatus
+      // 
+      cboStatus.FormattingEnabled = true;
+      cboStatus.Items.AddRange(new object[] { "All", "open", "disputed", "inactive", "resolved" });
+      cboStatus.Location = new Point(70, 17);
+      cboStatus.Name = "cboStatus";
+      cboStatus.Size = new Size(153, 27);
+      cboStatus.TabIndex = 2;
+      // 
+      // button2
+      // 
+      button2.Location = new Point(229, 12);
+      button2.Name = "button2";
+      button2.Size = new Size(132, 34);
+      button2.TabIndex = 1;
+      button2.Text = "送出查詢指令";
+      button2.UseVisualStyleBackColor = true;
+      button2.Click += button2_Click;
       // 
       // button1
       // 
-      button1.Location = new Point(237, 12);
+      button1.Location = new Point(656, 12);
       button1.Name = "button1";
       button1.Size = new Size(132, 34);
       button1.TabIndex = 0;
@@ -63,23 +108,20 @@
       webAgent.CreationProperties = null;
       webAgent.DefaultBackgroundColor = Color.White;
       webAgent.Dock = DockStyle.Fill;
-      webAgent.Location = new Point(0, 70);
+      webAgent.Location = new Point(0, 90);
       webAgent.Margin = new Padding(8);
       webAgent.Name = "webAgent";
       webAgent.Padding = new Padding(8);
-      webAgent.Size = new Size(800, 380);
+      webAgent.Size = new Size(800, 360);
       webAgent.TabIndex = 1;
       webAgent.ZoomFactor = 1D;
       // 
-      // button2
+      // progressBar
       // 
-      button2.Location = new Point(31, 17);
-      button2.Name = "button2";
-      button2.Size = new Size(94, 29);
-      button2.TabIndex = 1;
-      button2.Text = "送出指令";
-      button2.UseVisualStyleBackColor = true;
-      button2.Click += button2_Click;
+      progressBar.Location = new Point(70, 50);
+      progressBar.Name = "progressBar";
+      progressBar.Size = new Size(381, 10);
+      progressBar.TabIndex = 5;
       // 
       // Form1
       // 
@@ -92,6 +134,7 @@
       Text = "Form1";
       Load += Form1_Load;
       panel1.ResumeLayout(false);
+      panel1.PerformLayout();
       ((System.ComponentModel.ISupportInitialize)webAgent).EndInit();
       ResumeLayout(false);
     }
@@ -103,5 +146,9 @@
     private Microsoft.Web.WebView2.WinForms.WebView2 webAgent;
     private Button button1;
     private Button button2;
+    private ComboBox cboStatus;
+    private Label label1;
+    private Label labelResult;
+    private ProgressBar progressBar;
   }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,4 +1,5 @@
 using System.Net;
+using System.Reflection.Metadata;
 using System.Text.RegularExpressions;
 using System.Web;
 
@@ -13,6 +14,10 @@ namespace WinWebViewLab
 
     private async void Form1_Load(object sender, EventArgs e)
     {
+      // init
+      cboStatus.SelectedIndex = 0;
+
+      // init WebView2
       await webAgent.EnsureCoreWebView2Async(null);
       webAgent.CoreWebView2.Navigate("https://www.itfseafarers.org/en/abandonment-list/seafarer-abandonment");
     }
@@ -28,9 +33,50 @@ namespace WinWebViewLab
       string dataTable = decodedHtml2;
     }
 
-    private void button2_Click(object sender, EventArgs e)
+    private async void button2_Click(object sender, EventArgs e)
     {
+      labelResult.Text = string.Empty;
+      progressBar.Value = 0;
 
+      //string status = "disputed"; // All | open | disputed | inactive | resolved 。
+      string status = (string)(cboStatus.Text ?? "ALL");
+
+      string queryJs = $"document.querySelector('#edit-field-status-list-value').value = '{status}'; " 
+        + @"document.querySelector('#views-exposed-form-itf-current-cases-block-1').submit();";
+      await webAgent.CoreWebView2.ExecuteScriptAsync(queryJs);
+
+      progressBar.Value = 10;
+
+      // 讓子彈飛
+      await Task.Delay(1000);
+      progressBar.Value = 20;
+      await Task.Delay(1000);
+      progressBar.Value = 30;
+      await Task.Delay(1000);
+      progressBar.Value = 40;
+      await Task.Delay(1000);
+      progressBar.Value = 50;
+      await Task.Delay(1000);
+      progressBar.Value = 60;
+
+      // 檢畫面確定status 查詢條件無誤!
+      string regionResult = await webAgent.CoreWebView2.ExecuteScriptAsync(@"document.querySelector('#edit-field-region-target-id').value");
+      string statusResult = await webAgent.CoreWebView2.ExecuteScriptAsync(@"document.querySelector('#edit-field-status-list-value').value");
+      labelResult.Text = $"{regionResult} | {statusResult}";
+
+      progressBar.Value = 70;
+
+      // 取回資訊 html 或直接用 js 取值出來
+      string html = await webAgent.CoreWebView2.ExecuteScriptAsync(@"document.querySelector('#block-views-block-itf-current-cases-block-1 > div > div > table > tbody').outerHTML");
+      string decodedHtml = Regex.Unescape(html); // 解 Unicode, \u003C => `<`
+      string decodedHtml2 = WebUtility.HtmlDecode(decodedHtml); // 解 
+
+      progressBar.Value = 80;
+
+      // 解析資訊 => datainfo with RegEx 
+      string dataTable = decodedHtml2;
+
+      progressBar.Value = 100;
     }
   }
 }
